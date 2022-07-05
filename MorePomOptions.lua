397ccc56ff7e54f5f166e211b52345b529e2e136
Damian <damixd02@gmail.com>
2022-07-05 20:38:40 +0200

    Fixed criticial error in trait selection

--- MorePomOptions.lua
+++ MorePomOptions.lua
@@ -18,8 +18,7 @@ end)
 ModUtil.BaseOverride ( "GetAllUpgradeableGodTraits", function ( )
     local traitNames = {}
 	for _, trait in pairs( CurrentRun.Hero.Traits ) do
- 		if trait.RemainingUses == nil and (IsGodTrait(trait.Name) or TraitData[trait.Name].Icon ~= nil 
-		or string.find(trait.Name, "ChaosBlessing")) then
+ 		if trait.RemainingUses == nil and IsGodTrait(trait.Name) or string.find(trait.Name, "ChaosBlessing") then
 			traitNames[trait.Name] = true
         end
 	end
